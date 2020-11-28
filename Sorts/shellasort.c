4eae1f539ec01acfdc8a4b8098728151bb45891e
petrushka35 <61370310+petrushka35@users.noreply.github.com>
2020-11-28 18:21:32 +0700

    Update shellasort.c

--- Sorts/shellasort.c
+++ Sorts/shellasort.c
@@ -23,18 +23,39 @@ long time_stop()
 
 void my_shellsort(int * array, int N)
 {
-	int step = N/2, tmp;
-	for(step; step>0; step /= 2)
+	int inc[100]; 
+
+	int p1 = 1, p2 = 1, p3 = 1;
+	int i = -1;
+	do{
+  		if(++i % 2) 
+    		inc[i] = 8*p1 - 6*p2 + 1;
+  		else
+  		{
+    		inc[i] = 9*p1 - 9*p3 + 1;
+    		p2 *= 2;
+    		p3 *= 2;
+  		}
+  		p1 *= 2;
+  	}while(3*inc[i] <= N);
+
+	if(i > 0)
 	{
-		for(int i = step; i < N; i++)
-		{
-			for (int j = i - step; j >= 0 && array[j] > array[j + step]; j -= step)
+  		--i;
+  		while(i>=0)
+  		{
+  			int step = inc[--i];
+			int tmp;
+			for(int i = step; i < N; i++)
 			{
-				tmp = array[j];
-				array[j] = array[j + step];
-				array[j + step] = tmp;
+				for (int j = i - step; j >= 0 && array[j] > array[j + step]; j -= step)
+				{
+					tmp = array[j];
+					array[j] = array[j + step];
+					array[j + step] = tmp;
+				}
 			}
-		}
+  		}
 	}
 }
 
@@ -71,7 +92,7 @@ int main(int argc, char * argv[])
 	}
 	printf("%d", array[N-1]);
 
-	free(array);
 	fclose(fp);
+	free(array);
 	return 0;
 }
